83d6fb6028ffce00c2988cc875d49b6f561ff6fa
caitleigh <caitleigh27@gmail.com>
2020-11-10 18:01:28 -0800

    started styling landing page

---
+++ src/components/App/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './App.css';
+
+//components 
+
+import LandingPage from '../LandingPage/LandingPage'
+// import Footer from '../Footer/Footer'
+
+function App() {
+  return (
+    <>
+      {/* <p>/ / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / /</p>
+
+      <h1>Portfolio Under Construction</h1>
+      
+      <p>/ / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / /</p> */}
+      <LandingPage/>
+
+    {/* <Footer/> */}
+
+    </>
+
+  );
+}
+
+export default App;
