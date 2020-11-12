9c2491155f05bb9a447ddf41b56447cf3fb7a83c
caitleigh <caitleigh27@gmail.com>
2020-11-12 15:35:06 -0800

    landing page animations completed, still need to work on title font size to be more responsive and have view work button transition to new component

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -1,58 +1,28 @@
 import React from 'react';
 import './LandingPage.css'
 
-// const LandingPage = () => {
-//     return (
-//         <>
-//             <section className="title">
-//                 <h1>Rougeland</h1>
-//                 <h2>Productions</h2>
-//             </section>
-
-//             <section className="view-work">
-//                 <p>View Work</p>
-//             </section>
-
-//         </>
-//     )
-// }
-
-
-
-/*
- * A simple React component
- */
 class LandingPage extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            animationClass: 'gradient'
-        }
-        this.changeState = this.changeState.bind(this);
-    }
-
-    changeState() {
-        if (this.state.animationClass === 'gradient') {
-            this.setState({
-                animationClass: 'test paused'
-            });
-        } else {
-            this.setState({
-                animationClass: 'gradient'
-            });
-        }
-    }
     render() {
-        return <div className={this.state.animationClass}>
-            <section className="title">
-                <h1>Rougeland</h1>
-                <h2>Productions</h2>
-            </section>
+        return (
+            <>
+                <div className="gradient">
+                </div>;
+                    <div className="title">
+                        <h1>Rogueland</h1>
+                        <h2>Web Designs</h2>
+                        <h3>By Cait Rowland</h3>
+                    </div>
 
-            <section className="view-work">
-                <p>View Work</p>
-            </section>
-        </div>;
+                    <div className="view-work-container">
+                        <section className="view-work">
+                        <button>View Work</button>
+                        </section>
+                        <section className="view-work-line">
+
+                        </section>
+
+                    </div>
+            </>)
     }
 }
 export default LandingPage
