b479f1896f0828bda0d300296b8005b1c2d9d06b
caitleigh <caitleigh27@gmail.com>
2020-11-18 13:56:31 -0800

    updated deployed link to custom domain, added media query for title size, updated tab icon

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -1,27 +1,48 @@
 import React from 'react';
+import { BrowserRouter, Route, Redirect } from 'react-router-dom'
+
 import './LandingPage.css'
 
 class LandingPage extends React.Component {
+
+    state = {
+        redirect: false
+    }
+
+    setRedirect = () => {
+        this.setState({
+            redirect: true
+        })
+    }
+
+    renderViewWork = () => {
+        if (this.state.redirect) {
+            return <Redirect to='../ViewWork/ViewWork' />
+        }
+    }
+
     render() {
         return (
             <>
                 <div className="gradient">
                 </div>;
-                    <div className="title">
-                        <h1>Rogueland</h1>
-                        <h2>Web Designs</h2>
-                        <h3>By Cait Rowland</h3>
-                    </div>
+                <div className="title">
+                    <h1>Rogueland</h1>
+                    <h2>Web Designs</h2>
+                    <h3>By Cait Rowland</h3>
+                </div>
 
-                    <div className="view-work-container">
-                        <section className="view-work">
-                        <button>View Work</button>
-                        </section>
-                        <section className="view-work-line">
+                <div className="view-work-container" href='../ViewWork/ViewWork.js'>
+                    <section
+                        className="view-work">
 
-                        </section>
+                        <button onClick={this.setRedirect}>Under Construction</button>
+                    </section>
+                    <section className="view-work-line">
 
-                    </div>
+                    </section>
+
+                </div>
             </>)
     }
 }
