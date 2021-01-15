08c9fc9c435ba597d6bf9d670d76a3b31c9d57d5
caitleigh <caitleigh27@gmail.com>
2021-01-14 16:49:22 -0800

    got started on full view of about

--- src/components/Tiles/About/About.js
+++ src/components/Tiles/About/About.js
@@ -1,25 +1,64 @@
-import React from 'react'
+import React from "react";
 
-import './About.css'
+import "./About.css";
 
-//Build Preview 
+//Build Preview
 
 //Build Full View
 
-//Add on Click Functionality 
+//Add on Click Functionality
 
 const About = () => {
+  // return (
 
-    return (
+  //     //PREVIEW
+  //     <div className="about-prev-container">
+  //         <h4>About Cait</h4>
+  //         <div className="headshot" />
+  //         <p>Learn More</p>
+  //     </div>
+  // )
 
-        //PREVIEW 
-        <div className="about-container">
-            <h4>About Cait</h4>
-            <div className="headshot" />
-            <p>Learn More</p>
-        </div>
-    )
-    
-}
+  return (
+    //FULL VIEW
+    <div className="about-full-container">
+      <div className="full-view-left">
+        <h3>About Cait</h3>
+        <div className="headshot"></div>
+      </div>
+      <div className="full-view-right">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Fringilla
+          urna porttitor rhoncus dolor purus non. Velit ut tortor pretium
+          viverra suspendisse potenti. Sapien eget mi proin sed libero enim sed
+          faucibus turpis. Aliquam ultrices sagittis orci a scelerisque purus
+          semper eget. Arcu non sodales neque sodales ut etiam sit amet. Vivamus
+          at augue eget arcu dictum varius duis at. Quisque egestas diam in arcu
+          cursus euismod quis viverra. Nisl nisi scelerisque eu ultrices vitae.
+          Ut aliquam purus sit amet luctus venenatis lectus magna fringilla. Sed
+          turpis tincidunt id aliquet risus feugiat in ante. Nulla pellentesque
+          dignissim enim sit amet. Blandit aliquam etiam erat velit scelerisque
+          in dictum. Elit scelerisque mauris pellentesque pulvinar pellentesque
+          habitant morbi. Quam elementum pulvinar etiam non. Et netus et
+          malesuada fames ac turpis egestas sed. Id neque aliquam vestibulum
+          morbi blandit. Nulla aliquet enim tortor at auctor urna nunc id
+          cursus. Sit amet nulla facilisi morbi tempus iaculis urna id volutpat.
+          Diam in arcu cursus euismod quis viverra nibh cras pulvinar. Ornare
+          arcu odio ut sem nulla. Ornare suspendisse sed nisi lacus. Consectetur
+          adipiscing elit ut aliquam purus sit amet luctus venenatis. Volutpat
+          sed cras ornare arcu. A diam sollicitudin tempor id eu. Justo laoreet
+          sit amet cursus sit amet dictum sit amet. Libero justo laoreet sit
+          amet cursus sit amet. Commodo sed egestas egestas fringilla phasellus
+          faucibus scelerisque. Donec et odio pellentesque diam volutpat commodo
+          sed. Cras fermentum odio eu feugiat. Diam maecenas sed enim ut sem
+          viverra aliquet. Vitae justo eget magna fermentum iaculis eu non diam.
+          Non tellus orci ac auctor. Vitae nunc sed velit dignissim sodales ut
+          eu. Sed nisi lacus sed viverra tellus. Sit amet aliquam id diam.
+        </p>
+      </div>
+    </div>
+  );
+};
 
-export default About
+export default About;
